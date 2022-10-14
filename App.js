c7335978adf9b6b5a417164eb6029e9030c038b5
Agiannidis_Antonis <agiannidis.antonis@gmail.com>
2022-10-14 11:20:20 +0300

    expo v1

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Fragment, useState, useEffect} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Platform, ActivityIndicator } from 'react-native';
+import WebView from 'react-native-webview';
+/*import firebase from 'expo-firebase';*/
+/*import messaging from 'react-native-firebase';*/
+import messaging from '@react-native-firebase/messaging';
+import {Permissions, Notifications} from 'expo'
 
-export default function App() {
+
+const App = () => {
+
+/*  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }*/
+
+  //  https://staging.myrestaurant.gr/
+  //  https://admin.myrestaurant.gr/
+  //  http://localhost:3000/
+
+/*  window.onmessage = function(event) {
+    alert(event.data);
+  };*/
+
+  //document.getElementById('targetFrame').contentWindow.postMessage('data','*');
+
+  // document.querySelector("#targetFrame").contentWindow.test_funk()
+
+
+  useEffect(() => {
+    alert(1)
+  }, []);
+
+  const [contentLoaded , setContentLoaded] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        {Platform.OS === "web"
+            ?
+            <iframe id='targetFrame' src="https://staging.myrestaurant.gr" name="ios-app" height={'100%'} width={'99.99%'} />
+            :
+            <View style={{display:'flex', width:'100%',height:'100%'}}>
+                {!contentLoaded &&
+                <ActivityIndicator size="small" color="red" /> }
+                <WebView
+                    source={{uri: 'https://staging.myrestaurant.gr'}}
+                    style={{marginTop: 22, flex: 1}}
+                    startInLoadingState={true}
+                    injectedJavaScript={console.log('eo')}
+                    onLoad={() => setContentLoaded(true)}
+                    cacheEnabled={false}
+                    userAgent={'native'}
+                />
+            </View>
+
+        }
+
+      </SafeAreaView>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: Platform.OS === 'android' ? 25 : 0
   },
 });
+
+
+export default App
